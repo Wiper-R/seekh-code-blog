a1494f36fa3a0fc2c683707f04642bd7f34afef9
Shivang Rathore <rshivang12345@gmail.com>
2024-09-06 20:58:44 +0530

    chore: add nextauth

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUpForm } from "./sign-up-form";
-
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center p-20">
-      <SignUpForm />
-    </div>
-  );
-}
