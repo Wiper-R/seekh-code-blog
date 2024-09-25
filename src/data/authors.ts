abe94158d46e29dc068ed82d3257ae2baab8cc1f
Shivang Rathore <rshivang12345@gmail.com>
2024-09-25 08:58:17 +0530

    chore: add authors and basic post styling

---
+++ src/data/authors.ts
@@ -0,0 +1,13 @@
+type Author = {
+  name: string;
+  website: string;
+  avatar: string;
+};
+
+export const authors: { [k: string]: Author } = {
+  wiperr: {
+    name: "Shivang Rathore",
+    website: "https://github.com/wiper-r",
+    avatar: "/avatars/wiperr.png",
+  },
+};
