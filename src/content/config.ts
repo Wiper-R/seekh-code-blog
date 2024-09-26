73bf4cf0d1fae503986081b28d8f274e8d5a8393
Shivang Rathore <rshivang12345@gmail.com>
2024-09-26 20:22:02 +0530

    refactor: project structure and image placement

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,15 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    authorId: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image(),
+      authorId: z.string(),
+    }),
 });
 
 export const collections = { blog };
