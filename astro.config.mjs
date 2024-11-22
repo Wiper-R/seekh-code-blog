aefcc6f52ddba261a721402551777a31fa35dec1
Shivang <rshivang12345@gmail.com>
2024-11-22 09:55:13 +0530

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,18 @@ import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "http://seekhcode.me",
+  site: "https://seekhcode.me",
   // base: "/seekh-code-blog",
   integrations: [
     tailwind(),
     react({ experimentalReactChildren: true }),
     mdx(),
     partytown({ config: { forward: ["dataLayer.push"] } }),
+    sitemap()
   ],
   markdown: {
     shikiConfig: { theme: "slack-dark" },
